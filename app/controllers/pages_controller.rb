67d8b655a47aba92d82263832b0ac32d244b49f1
hlerman <haguyle@gmail.com>
2018-06-14 01:10:05 -0400

    added a route and a method in the controller for contest page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,12 @@ class PagesController < ApplicationController
 
      def welcome
           render :welcome
-
      end
      def about
           render :about
      end
-
+     def contest
+          render :contest
+     end
 
 end
