2e21721da222f2d99b6c7f9482fa642e9a8e60b3
hlerman <haguyle@gmail.com>
2018-06-14 01:15:30 -0400

    added @header from app/views/layouts/application.html.erb to every method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,15 @@ class PagesController < ApplicationController
 
      def welcome
           render :welcome
+          @header = "This is the welcome page"
      end
      def about
           render :about
+          @header = "This is the about us page"
      end
      def contest
           render :contest
+          @header = "This is the contest page"
      end
 
 end
