4226ba7f79b622e7152a7762410fe8a4207db8c6
hlerman <haguyle@gmail.com>
2018-06-14 09:38:51 -0400

    defining a dynamic routes to kitten page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ get '/' => 'pages#welcome'
 get '/welcome' => 'pages#welcome'
 get '/about' => 'pages#about'
 get '/contest' => 'pages#contest'
+get '/kitten/:size' => 'pages#kitten'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 # get '/welcome' => 'pages#welcome'
